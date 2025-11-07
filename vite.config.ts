8cac15038cf88ca48599c34919d1b6fa0e2f8b37
RonalAR <ronaldayra1617@gmail.com>
2025-11-06 20:55:26 -0400

    adds copy scene

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/ronal_ayra_revollo/",
   plugins: [react()],
+  publicDir: "public",
+
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
